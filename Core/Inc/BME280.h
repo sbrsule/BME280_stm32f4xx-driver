7f5d85cda293939b3e59526d9744df19cf11fb35
sbrsule <sbrsuleim@gmail.com>
2023-04-29 22:29:22 -0400

    added sensor config

--- Core/Inc/BME280.h
+++ Core/Inc/BME280.h
@@ -6,9 +6,11 @@
 */
 #define BME280_ADDRESS              0x77 << 1
 #define BME280_ID_ADDRESS           0xD0
+#define BME280_CTRL_HUM_ADDRESS     0xF2
 #define BME280_CTRL_MEAS_ADDRESS    0xF4
 #define BME280_SOFTRESET_ADDRESS    0xE0
 #define BME280_CONFIG_ADDRESS       0xF5
+#define BME280_RESET_ADDRESS        0xE0
 
 /**
 *   CALIBRATION REGISTER ADDRESSES
@@ -20,10 +22,86 @@
 /**
 *   POWER MODES
 */
-#define BME280_SLEEP_MODE   0x00
-#define BME280_FORCED_MODE  0x01
-#define BME280_NORMAL_MODE  0x03
-#define BME280_SOFT_RESET   0xB6
+typedef enum PowerMode {
+    SLEEP_MODE  = 0x00;
+    FORCED_MODE = 0x01;
+    NORMAL_MODE = 0x03;
+} PowerMode;
 
+/**
+*   HUMIDITY OVERSAMPLING SETTINGS
+*/
+typdef enum OsrsH {
+    H_OFF   = 0b000;
+    H_1x    = 0b001;
+    H_2x    = 0b010;
+    H_4x    = 0b011;
+    H_8x    = 0b100;
+    H_16x   = 0b101;
+} OsrsH;
+
+/**
+*   TEMPERATURE OVERSAMPLING SETTINGS
+*/
+typedef enum OsrsT {
+    T_OFF   = 0b000 << 5;
+    T_1x    = 0b001 << 5;
+    T_2x    = 0b010 << 5;
+    T_4x    = 0b011 << 5;
+    T_8x    = 0b100 << 5;
+    T_16x   = 0b101 << 5;
+} OsrsT;
+
+/**
+*   PRESSURE OVERSAMPLING SETTINGS
+*/
+typedef enum OsrsP {
+    P_OFF   = 0b000 << 2;
+    P_1x    = 0b001 << 2;
+    P_2x    = 0b010 << 2;
+    P_4x    = 0b011 << 2;
+    P_8x    = 0b100 << 2;
+    P_16x   = 0b101 << 2;
+} OsrsP;
+
+/**
+*   TIME BETWEEN MEASUREMENTS (DURING NORMAL MODE)
+*/
+typedef enum StandByTime {
+    MS_HALF = 0b000 << 5;
+    MS_62   = 0b001 << 5;
+    MS_125  = 0b010 << 5;
+    MS_250  = 0b011 << 5;
+    MS_500  = 0b100 << 5;
+    MS_1000 = 0b101 << 5;
+    MS_10   = 0b110 << 5;
+    MS_20   = 0b111 << 5;
+} StandByTime;
+
+/**
+*   IRR FILTER TIME CONSTANT SETTINGS
+*/
+typdef enum FilterIRR {
+    IRR_OFF = 0b000 << 2;
+    IRR_2   = 0b001 << 2;
+    IRR_4   = 0b010 << 2;
+    IRR_8   = 0b011 << 2;
+    IRR_16  = 0b100 << 2;
+}
+
+typedef struct BME280
+{
+    I2C_HandleTypeDef* i2c_handle;
+    uint16_t dig_T1;
+    int16_t dig_T2;
+    int16_t dig_T3;
+} BME280;
+
+typedef enum BME280_error
+{
+    E_SUCCESS       = 0,
+    E_INCORRECT_ID  = 1,
+    E_I2C           = 2,
+} BME280_error;
 
 #endif 
--- Core/Src/BME280.c
+++ Core/Src/BME280.c
@@ -2,3 +2,57 @@
 #include "BME280.h"
 #include "main.h"
 
+BME280_error BME280_init(BME280* dev, I2C_HandleTypeDef* i2c_handle)
+{
+    BME280_error error_status;
+    dev->i2c_handle = i2c_handle;
+
+    // Check chip ID of sensor, should be 0x60
+    error_status = validate_id(dev);
+    if (error_status) {
+        return id_status;
+    } else if (dev->id != 0x60) {
+        return E_INCORRECT_ID;
+    }
+
+    // Reset sensor
+    error_status = reset(dev);
+    if (error_status) return error_status;
+
+    error_status = config(dev);
+    if (error_status) return error_status;
+}
+
+static BME280_error validate_id(BME280* dev)
+{
+    uint8_t id, hal_status;
+    hal_status = HAL_I2C_Mem_Read(dev->i2c_handle, BME280_ADDRESS, BME280_ID_ADDRESS, I2C_MEMADD_SIZE_8BIT, &id, sizeof(id), HAL_MAX_DELAY);
+    dev->id = id;
+    if (hal_status != HAL_OK) {
+        return E_I2C;
+    }
+    return E_SUCCESS
+}
+
+static BME280_error reset(BME280* dev)
+{
+    uint8_t hal_status = HAL_I2C_Mem_Write(dev->i2c_handle, BME280_ADDRESS, BME280_RESET_ADDRESS, I2C_MEMADD_SIZE_8BIT, BME280_SOFT_RESET, sizeof(BME280_SOFT_RESET), HAL_MAX_DELAY);
+    if (hal_status != HAL_OK) return E_I2C;
+    return E_SUCCESS;
+}
+
+static BME280_error config(BME280* dev, OsrsH osrs_h, OsrsT osrs_t, OsrsP osrs_p, PowerMode mode, StandByTime time, FilterIRR irr)
+{
+    uint8_t hal_status = 0;
+    uint8_t data = OsrsH;
+    hal_status |= HAL_I2C_Mem_Write(dev->i2c_handle, BME280_ADDRESS, BME280_CTRL_HUM_ADDRESS, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), HAL_MAX_DELAY);
+
+    data = OsrsT | OsrsP | PowerMode;
+    hal_status |= HAL_I2C_Mem_Write(dev->i2c_handle, BME280_ADDRESS, BME280_CTRL_MEAS_ADDRESS, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), HAL_MAX_DELAY);
+    
+    data = time | irr;
+    hal_status |= HAL_I2C_Mem_Write(dev->i2c_handle, BME280_ADDRESS, BME280_CONFIG_ADDRESS, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), HAL_MAX_DELAY);
+
+    if (hal_status) return E_I2C;
+    return E_SUCCESS;
+}
